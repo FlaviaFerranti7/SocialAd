85332718ea5e1d72876bb25e4421d66491906317
flavia.ferranti <ferranti.1707897@studenti.uniroma1.it>
2021-03-24 15:04:15 +0100

    add: toolbar

--- app/src/main/java/com/example/socialad/MainActivity.kt
+++ app/src/main/java/com/example/socialad/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.example.socialad
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle;
+
+    @SuppressLint("RestrictedApi")
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mToolbar : Toolbar = main_page_toolbar as Toolbar
+        setSupportActionBar(mToolbar);
+        supportActionBar?.title = "Home";
+
         val headerView: View = navigation_view.inflateHeaderView(R.layout.navigation_header);
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.drawer_open, R.string.drawer_close);
+        drawer_layout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         navigation_view.setNavigationItemSelectedListener {
             when(it.itemId){
@@ -49,6 +67,13 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle?.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
